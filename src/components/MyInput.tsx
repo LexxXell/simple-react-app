84e5e1950b26c8c1107f5b564383f5cd0e738f0a
LexxXell <lexxxell007@gmail.com>
2023-11-23 18:41:36 +0400

    Update

--- src/components/MyInput.tsx
+++ src/components/MyInput.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, useCallback, useState } from 'react';
 import './MyInput.css';
 import { IMyComponentProps } from '../@types';
 
@@ -13,15 +13,23 @@ interface IMyInputProps extends IMyComponentProps {
 const MyInput: React.FC<IMyInputProps> = ({ placeholder, type = 'text', onChange }) => {
   const [isShow, setIsShow] = useState(false);
 
-  const handleClickShow = () => setIsShow(!isShow);
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => onChange?.(event.target.value);
+  const handleClickShow = useCallback(() => setIsShow(!isShow), [isShow]);
+
+  const handleChange = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      const { value } = event.target;
+      onChange?.(value);
+    },
+    [onChange],
+  );
 
   const inputType: InputType = type === 'text' ? 'text' : isShow ? 'text' : 'password';
+  const show = isShow ? 'show' : '';
 
   return (
     <div className="input-wrapper">
       <input type={inputType} placeholder={placeholder} onChange={handleChange} />
-      <div className={`show-pass-icon ${isShow ? 'show' : ''} ${type}`} onClick={handleClickShow}>
+      <div className={`show-pass-icon ${show} ${type}`} onClick={handleClickShow}>
         <svg width="18" height="18" xmlns="http://www.w3.org/2000/svg">
           <path d="M8.86298 6.14997L11.4397 8.72665L11.4519 8.59168C11.4519 7.23789 10.3517 6.1377 8.99795 6.1377L8.86298 6.14997Z" />
           <path d="M8.99795 4.50103C11.2556 4.50103 13.0879 6.33335 13.0879 8.59102C13.0879 9.11863 12.9816 9.6217 12.7976 10.0839L15.1902 12.4765C16.4254 11.4458 17.3988 10.1125 18 8.59102C16.5808 5.00003 13.092 2.45605 8.99798 2.45605C7.85278 2.45605 6.75669 2.66054 5.73828 3.02864L7.50515 4.79141C7.96727 4.61146 8.47034 4.50103 8.99795 4.50103Z" />
--- src/components/MyToggleSwitch.tsx
+++ src/components/MyToggleSwitch.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import './MyToggleSwitch.css';
 import { IMyComponentProps } from '../@types';
 
 interface IMyToggleSwitchProps extends IMyComponentProps {
   state0Text?: string;
   state1Text?: string;
-  currentState?: string;
-  onToggleChange?: (state: string) => {};
+  state?: string;
+  onToggleChange?: (state: 0 | 1) => void;
 }
 
 const MyToggleSwitch: React.FC<IMyToggleSwitchProps> = ({
@@ -15,19 +15,25 @@ const MyToggleSwitch: React.FC<IMyToggleSwitchProps> = ({
   className,
   state0Text = 'State 0',
   state1Text = 'State 1',
-  onToggleChange = console.log,
+  state: externalState,
+  onToggleChange,
 }) => {
-  const [state, setState] = useState(0);
+  const [state, setState] = useState(Number(externalState || 0));
 
-  const handleToggle = () => {
-    setState((prevState) => (prevState === 0 ? 1 : 0));
-    onToggleChange(state);
-  };
+  const handleToggle = useCallback(() => {
+    const nextState = state === 0 ? 1 : 0;
+    setState(nextState);
+    onToggleChange?.(nextState as 0 | 1);
+  }, [state, onToggleChange]);
+
+  useEffect(() => {
+    setState(Number(externalState || 0));
+  }, [externalState]);
 
   return (
     <div style={{ width, height }} className={`toggle-wrapper ${className}`} onClick={handleToggle}>
-      <div className={`toggle-text ${state ? '' : 'active'}`}>{state0Text}</div>
-      <div className={`toggle-text ${state ? 'active' : ''}`}>{state1Text}</div>
+      <div className={`toggle-text ${state === 0 ? 'active' : ''}`}>{state0Text}</div>
+      <div className={`toggle-text ${state === 1 ? 'active' : ''}`}>{state1Text}</div>
     </div>
   );
 };
