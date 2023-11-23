73f1fe048166420c9813c677dd9e23b5cf2ac742
LexxXell <lexxxell007@gmail.com>
2023-11-23 17:16:42 +0400

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import './App.css';
 import MyButton from './components/MyButton';
+import MyInput from './components/MyInput';
 import MyToggleSwitch from './components/MyToggleSwitch';
 
 function App() {
   return (
     <>
       <MyToggleSwitch width="329px" height="59px" state0Text="Login" state1Text="Register" />
-      <MyButton className="login-my-button">Login</MyButton>
+      <MyButton width="232" height="49" className="login-my-button">
+        Login
+      </MyButton>
+      <MyInput placeholder="Input something common" />
+      <MyInput placeholder="Input something secret" type="password" />
     </>
   );
 }
