fa8e1a5de96ee1158e7681e4ebe7c415b76ba952
LexxXell <lexxxell007@gmail.com>
2023-11-24 16:09:37 +0400

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import './App.css';
 import MyCatImage from './components/MyCatImage';
+
+import './App.css';
+import MyAuthForm from './components/MyAuthForm';
 
 function App() {
   return (
-    <>
+    <div className="container">
       <MyCatImage />
-    </>
+      <div className="auth-container">
+        <MyAuthForm width="435px" />
+      </div>
+    </div>
   );
 }
 
