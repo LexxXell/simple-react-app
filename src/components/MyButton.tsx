2f75f5c5a3858f29f316961113cf1aa72e000ce8
LexxXell <lexxxell007@gmail.com>
2023-11-24 14:27:11 +0400

    MyButton update

--- src/components/MyButton.tsx
+++ src/components/MyButton.tsx
@@ -3,15 +3,16 @@ import './MyButton.css';
 import { IMyComponentProps } from '../@types';
 
 interface IMyButtonProps extends IMyComponentProps {
+  view?: 'def' | 'alt';
   onClick?: () => void;
   children?: React.ReactNode;
 }
 
-const MyButton: React.FC<IMyButtonProps> = ({ width, height, className, onClick, children }) => {
+const MyButton: React.FC<IMyButtonProps> = ({ width, height, className, onClick, children, view = '' }) => {
   const handleClick = () => onClick?.();
 
   return (
-    <button className={`my-button ${className}`} style={{ width, height }} onClick={handleClick}>
+    <button className={`my-button ${className} ${view}`} style={{ width, height }} onClick={handleClick}>
       {children}
     </button>
   );
